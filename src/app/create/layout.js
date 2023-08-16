51b138fddc3fc89235893bd1b042ead5d2af83ed
chochojj <csgy114@gmail.com>
2023-08-16 16:31:32 +0900

    글 생성

--- src/app/create/layout.js
+++
@@ -1,8 +0,0 @@
-export default function Layout(props) {
-  return (
-    <>
-      <h2>Create</h2>
-      {props.children}
-    </>
-  );
-}
--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,3 +1,35 @@
+"use client";
+
 export default function Create() {
-  return <>Create!</>;
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const title = e.target.title.value;
+        const body = e.target.body.value;
+        const options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ title, body }),
+        };
+        fetch(`http://localhost:9999/topics`, options)
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+          });
+      }}
+    >
+      <p>
+        <input type="text" name="title" placeholder="title" />
+      </p>
+      <p>
+        <textarea name="body" placeholder="body" />
+      </p>
+      <p>
+        <input type="submit" value="create" />
+      </p>
+    </form>
+  );
 }
